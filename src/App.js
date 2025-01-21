85efbc0c80ec67d0b36cdd27d86c5f647309f68c
= <=>
2025-01-21 17:37:50 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import DebounceComponent from './Components/DebounceComponent';
-import PaginationComponent from './Components/PaginationComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import DebounceComponent from "./Components/DebounceComponent";
+import PaginationComponent from "./Components/PaginationComponent";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import HomeComponent from "./Route.js/HomeComponent";
+import AboutComponent from "./Route.js/AboutComponent";
 
 function App() {
   return (
     <div className="App">
       {/* <DebounceComponent />  */}
-      <PaginationComponent />
+      {/* <PaginationComponent /> */}
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomeComponent />} />
+            <Route path="/about" element={<AboutComponent />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
